b29cb956c1d15ccb6756c5d9814337d51153ae47
Ryan Newton <vonkunesnewton@gmail.com>
2017-04-18 10:21:29 -0700

    Add offset

--- convert_posts.py
+++ convert_posts.py
@@ -4,6 +4,12 @@ from medium_import import open_medium_import_for_links
 
 username = raw_input("What's your tumblr username: ")
 xsrf_token = raw_input("What's the xsrf token from medium?: ")
+offset = raw_input("Any offset(how many of these have you migrated before)?: ") or 0
 
 blog_links = get_all_blog_links_for_user(username)
-open_medium_import_for_links(blog_links[::-1], xsrf_token)  # reversing here, so we can post in order of age
+
+if offset: 
+    blog_links = blog_links[:-int(offset) + 1]
+blog_links = blog_links[::-1] # reversing here, so we can post in order of age
+
+open_medium_import_for_links(blog_links, xsrf_token)  
